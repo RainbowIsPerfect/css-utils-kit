6b87e6b2565f0b40e5ba90280b8e8a1e9a589d65
RainbowIsPerfect <andrey-smollin@rambler.ru>
2024-06-22 18:34:56 +0300

    borders update

--- src/breakpoints.ts
+++
@@ -1,9 +0,0 @@
-export const breakpoints = {
-  "--breakpoint-xs": "20em",
-  "--breakpoint-sm": "30em",
-  "--breakpoint-md": "48em",
-  "--breakpoint-lg": "62em",
-  "--breakpoint-xl": "75em",
-  "--breakpoint-2xl": "90em",
-  "--breakpoint-3xl": "120em",
-};
